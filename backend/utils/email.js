18302fc6ffc3599bc05b5977d12066f7e70bf756
Kristine Villanueva <kov2023-7058-45144@bicol-u.edu.ph>
2026-08-30 14:01:28 +0800

    fix

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -3,13 +3,13 @@ const nodemailer = require("nodemailer");
 // Updated transporter for Render to force IPv4
 const transporter = nodemailer.createTransport({
    host: 'smtp.gmail.com',
-   port: 465,
-   secure: true,
+   port: 587,
+   secure: false,
    auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
    },
-   family: 4 // This forces Nodemailer to use IPv4 and bypasses the Render IPv6 block
+   family: 4
 });
 
 async function sendReminderEmail(to, bookTitle, dueDate) {
